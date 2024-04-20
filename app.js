ed5fc6d58bf1e396f1b7c82290b69e57243c7755
rahul <rahulkaushal@pec.edu.in>
2024-04-21 00:41:48 +0530

    updated webhook

--- app.js
+++ app.js
@@ -32,8 +32,9 @@ const whitelist = [
 ];
 
 app.use((req, res, next) => {
-    if (req.path === '/webhook') {
+    if (req.path === '/order/webhook') {
         // Ensure the raw body is used for Stripe webhook verification
+        console.log(req.path);
         express.raw({ type: 'application/json' })(req, res, (err) => {
             if (err) {
                 return next(err);
